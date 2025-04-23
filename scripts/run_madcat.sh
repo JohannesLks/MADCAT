5720f39596f59143f12134d531e9e193960f9b88
JohannesLks <lukasjohannesmoeller@protonmail.com>
2025-04-24 01:00:01 +0200

    debug

--- scripts/run_madcat.sh
+++ scripts/run_madcat.sh
@@ -3,7 +3,7 @@
 sudo iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 1:65534 -j DNAT --to 192.168.1.100:65535
 sudo iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP
 # Start Enrichment Processor, piping results to /data/madcat.log
-sudo /usr/bin/python3 /opt/madcat/enrichment_processor.py /etc/madcat/config.lua  2>>/data/error.enrichment.log 1>>/data/madcat.log &
+sudo /usr/bin/python3 /opt/madcat/enrichment_processor.py /etc/madcat/config.lua  2>>/data/error.enrichment.log 1>>/data/enriched.json.log &
 # Give Enrichment Processor time to start up and open /tmp/logs.erm as configured
 sleep 1
 # Start UDP-, ICMP-, RAW-Module, let them pipe results to Enrichment Processor FIFO.
